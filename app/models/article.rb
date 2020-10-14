125047a05bc4881877033af416af8540c485adc7
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-14 23:23:00 +0100

    Add category index to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
   has_many :votes, dependent: :destroy
   has_one_attached :image
+  belongs_to :category, class_name: 'Category', foreign_key: :category_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_204306) do
+ActiveRecord::Schema.define(version: 2020_10_14_215735) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,7 +39,16 @@ ActiveRecord::Schema.define(version: 2020_10_14_204306) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "priority"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_204306) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "articles", "categories"
   add_foreign_key "articles", "users"
   add_foreign_key "votes", "articles"
   add_foreign_key "votes", "users"
