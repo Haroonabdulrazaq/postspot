481e4e9ab16fbf7714b981d713abc536561e4298
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-20 11:19:05 +0100

    Add test for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :articles
     has_many :votes
-
-    validates :name, presence: true
+    validates_presence_of :name, :password
+    validates_uniqueness_of :name
     has_secure_password
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+    factory :user do
+      name     { Faker::Name.name }
+      password { Faker::Internet.password }
+    end
+  end
