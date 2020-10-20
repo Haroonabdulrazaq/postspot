eafa20ce2f61de780cdf0d7d814e43a9105df38c
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-20 23:56:48 +0100

    Fix rubocop error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,7 @@ require 'capybara/rspec'
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# rubocop:disable Lint/ShadowingOuterLocalVariable
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -28,7 +29,7 @@ RSpec.configure do |config|
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
+# rubocop:enable Lint/ShadowingOuterLocalVariable
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
