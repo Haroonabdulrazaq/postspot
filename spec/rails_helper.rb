8a014d9d02fe1c13c50b5159b93d08640207a1fe
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-21 00:00:54 +0100

    Change shoulda matcher location

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,14 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'shoulda/matchers'
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,14 +68,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-# rubocop:disable Lint/ShadowingOuterLocalVariable
-  require 'shoulda/matchers'
-
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
-# rubocop:enable Lint/ShadowingOuterLocalVariable
 end
