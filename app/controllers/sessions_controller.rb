cd21a8af48d30a02150fd6bf9a21a7b3f5805a46
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-23 00:08:17 +0100

    Fix rubocop error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def new
     @session = @user
   end
-
+# ruboocop disable:Style/SafeNavigation
   def create
     @user = User.find_by(name: params[:session][:name])
 
-    if @user && @user.authenticate(params[:session][:password])
+    if @user&.@user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:notice] = "Welcome! #{@user.name}, you have signed in sucessfully"
       redirect_to root_path
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
       render :new
     end
   end
-
+# ruboocop enable:Style/SafeNavigation
   def destroy
     session.delete(:user_id)
     @current_user = nil
