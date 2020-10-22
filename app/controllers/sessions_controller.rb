cd2298cd2a44965eec6f2a64b5c8d39bd8aa10eb
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-22 23:45:03 +0100

    Add user authentication with Bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:session][:name])
 
-    if @user
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       flash[:notice] = "Welcome! #{@user.name}, you have signed in sucessfully"
       redirect_to root_path
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def logged_in_user
-    if session[:user_id].present?
-      User.find_by_id(session[:user_id]).name.to_s
-    else
+    if current_user.nil?
       'Welcome'
+    else
+      User.find_by_id(session[:user_id]).name.to_s
     end
   end
 
